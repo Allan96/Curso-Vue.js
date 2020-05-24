0436f1ce1cb3581ab0a18660ccac856525eaab2e
Allan96 <allan.souza.silva@usp.br>
2020-05-24 18:36:32 -0300

    Desafio 3

---
+++ dom/desafio-03/app.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0,
+        resultado: 'Valor Diferente'
+    },
+    watch: {
+        resultado() {
+            setTimeout(() => {
+                this.valor = 0;
+            }, 5000)
+        }
+    }
+});
